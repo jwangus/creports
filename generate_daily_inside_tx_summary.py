e1513a2c04777e97b84dbf62042f0a705df32b70
Jim Wang <jim.wang.nj@gmail.com>
2022-05-11 19:16:50 -0400

    added logic to save all trades and fixed a bug

--- generate_daily_inside_tx_summary.py
+++ generate_daily_inside_tx_summary.py
@@ -9,7 +9,7 @@ import pandas as pd
 if __name__ == '__main__':
     logging.basicConfig(filename=os.path.join(LOG_FOLDER, 'generate_daily_inside_tx_summary.log'),
                         format='%(asctime)s %(name)s %(levelname)s - %(message)s', level=logging.INFO)
-    logging.getLogger().setLevel(logging.WARNING)
+    logging.getLogger().setLevel(logging.WARN)
 
     log = logging.getLogger("insider_tx_summary")
     log.setLevel(logging.INFO)
@@ -19,19 +19,18 @@ if __name__ == '__main__':
         f"generate daily inside trading summray file for date: {report_date}")
 
     try:
-        log.info("download all form4 fillings")
-        download_form4(report_date)
         log.info("extracting report data")
         process_download(report_date)
         log.info("createing report data")
         rep_data = generate_daily_summary_report_data(report_date)
-        df = generate_daily_summary_report(rep_data)
+        df, df_raw = generate_daily_summary_report(rep_data)
 
         filename = os.path.join(
             REPORTS_FOLDER, f"insider_reports_{report_date}")
 
         log.info(f"saveing csv and html files: {filename}")
         df.to_csv(filename+".csv")
+        df_raw.to_csv(filename+"_raw.csv")
         with open(filename+".html", "w") as f:
             f.write(df.to_html(escape=False))
 
--- secfilings.py
+++ secfilings.py
@@ -70,34 +70,31 @@ def parse_form4_xml(s, filename):
             filter(lambda l: not l.startswith(("<XML>", "</XML>")), f.readlines()))
     logging.debug(str(root))
 
-    s["cik"].append(root.find(
-        "./issuer/issuerCik").text)
-    s["ticker"].append(root.find(
-        "./issuer/issuerTradingSymbol").text)
-    s["name"].append(root.find(
-        "./reportingOwner/reportingOwnerId/rptOwnerName").text)
+    cik = root.find("./issuer/issuerCik").text
+    ticker = root.find("./issuer/issuerTradingSymbol").text
+    name = root.find("./reportingOwner/reportingOwnerId/rptOwnerName").text
     relationship, title = map_relationship(root)
-    s["relationship"].append(relationship)
-    s["title"].append(title)
-    path_root = "./nonDerivativeTable/nonDerivativeTransaction/"
-    if root.find(path_root):
-        s["tx_date"].append(
-            dateutil.parser.isoparse(root.find(f"{path_root}transactionDate/value").text).date())
-        s["tx_code"].append(root.find(
-            f"{path_root}transactionCoding/transactionCode").text)
-        s["tx_share"].append(
-            float(root.find(f"{path_root}transactionAmounts/transactionShares/value").text))
-        e = root.find(
-            f"{path_root}transactionAmounts/transactionPricePerShare/value")
+    path_root = "./nonDerivativeTable/"
+    for ele in root.findall(path_root):
+        e_tx_date = ele.find("./transactionDate/value")
+        e_tx_code = ele.find("./transactionCoding/transactionCode")
+        if e_tx_date is None or e_tx_code is None:
+            continue
+
+        s["cik"].append(cik)
+        s["ticker"].append(ticker)
+        s["name"].append(name)
+        s["relationship"].append(relationship)
+        s["title"].append(title)
+        
+        s["tx_date"].append(dateutil.parser.isoparse(e_tx_date.text).date())
+        s["tx_code"].append(e_tx_code.text)
+        e = ele.find("./transactionAmounts/transactionShares/value")
+        s["tx_share"].append(None if e is None else float(e.text))
+        e = ele.find("./transactionAmounts/transactionPricePerShare/value")
         s["tx_price"].append(None if e is None else float(e.text))
-        s["share_post_tx"].append(
-            float(root.find(f"{path_root}postTransactionAmounts/sharesOwnedFollowingTransaction/value").text))
-    else:
-        s["tx_date"].append(None)
-        s["tx_code"].append(None)
-        s["tx_share"].append(None)
-        s["tx_price"].append(None)
-        s["share_post_tx"].append(None)
+        v = ele.find("./postTransactionAmounts/sharesOwnedFollowingTransaction/value").text
+        s["share_post_tx"].append(float(v))
 
 
 def generate_daily_summary_report_data(report_date):
@@ -107,24 +104,33 @@ def generate_daily_summary_report_data(report_date):
     s = {"cik": [], "ticker": [], "name": [], "relationship": [], "title": [],
          "tx_date": [], "tx_code": [], "tx_share": [], "tx_price": [], "share_post_tx": []}
 
+    # sometimes, identicle form 4 would appear under owner's CIK and company's CIK.
+    # So we want to filter out the duplicate.
+    folder_seen=[]
     for filename in all_form4_xml:
-        parse_form4_xml(s, filename)
+        folder_name = os.path.basename(os.path.dirname(filename))
+        if folder_name not in folder_seen:
+            folder_seen.append(folder_name)
+            parse_form4_xml(s, filename)
 
     return s
 
 
 def capitalize_word(n):
-    donot_cap_words = ["VP", "EVP", "CEO", "II", "III"]
+    donot_cap_words = ["VP", "EVP", "CEO", "II", "III", "CFO"]
     return ' '.join(i if i in donot_cap_words else i.capitalize() for i in n.split())
+
 
 def format_name_title(name_title):
     comp = name_title.split("/")
-    cik = comp[0] 
-    cell_text="/".join(comp[1:])
+    cik = comp[0]
+    cell_text = "/".join(comp[1:])
     return f'<a href="https://www.sec.gov/cgi-bin/own-disp?action=getissuer&CIK={cik}">{cell_text}</a>'
+
 
 def format_ticker(t):
     return f'<a href="https://finance.yahoo.com/quote/{t}">{t}</a>'
+
 
 def generate_daily_summary_report(report_data):
     df = pd.DataFrame(report_data)
@@ -133,10 +139,10 @@ def generate_daily_summary_report(report_data):
     df["sign_pre_share_calc"] = df["tx_code"].map(
         lambda c: 1 if c == "P" else -1)
     df["pre_share"] = df.share_post_tx - df.tx_share * df.sign_pre_share_calc
-    df["tx_share_percentage"] = df.tx_share / \
-        df.pre_share * 100 * df["sign_pre_share_calc"]
+    df["tx_share_percentage"] = 100*df["tx_share"] / \
+        df["pre_share"]*df["sign_pre_share_calc"]
 
-    summary = df.groupby(["cik", "name", "relationship", "title", "ticker", "tx_code", "sign_pre_share_calc"]).agg(
+    summary = df.groupby(["cik", "name", "relationship", "title", "ticker", "tx_code", "tx_date", "sign_pre_share_calc"]).agg(
         {"amt": ["sum"], "tx_share_percentage": ["max"], "share_post_tx": ["max"], "tx_price": ["max"]})
     summary.sort_values(["sign_pre_share_calc", ("amt", "sum")],
                         ascending=False, inplace=True)
@@ -152,6 +158,8 @@ def generate_daily_summary_report(report_data):
     df_output["Buy/Sell"] = summary["tx_code"].map(
         lambda c: "Sell" if c == "S" else "Buy")
 
+    df_output["Trade Date"] = summary["tx_date"].map(str)
+
     df_output["Trade Amount"] = summary[("amt", "sum")].map('{:,.0f}'.format)
     df_output["% Change In Position"] = summary[(
         "tx_share_percentage", "max")].map(lambda n: '{:,.01f}%'.format(n) if n != np.Inf else 'New')
@@ -161,4 +169,4 @@ def generate_daily_summary_report(report_data):
     df_output["Trade Price"] = summary[(
         "tx_price", "max")].map('{:,.2f}'.format)
 
-    return df_output
+    return df_output, pd.DataFrame(report_data)
